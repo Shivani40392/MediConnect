535ee01bc2ec1fda6f58d2c3c055d96665b914dc
shivani <shivanigupta40392@gmail.com>
2025-05-30 14:24:16 +0530

    banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
-import Banner from '../components/banner'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
