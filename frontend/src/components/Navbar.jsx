329617c4e2ba7a099540dba5cfda013a2127796b
shivani <shivanigupta40392@gmail.com>
2025-05-31 12:39:55 +0530

    admin changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,7 +39,10 @@ const Navbar = () => {
       <div className='flex items-center gap-4'>
         {
           token && userData ? <div className='flex items-center gap-2 cursor-pointer group relative' >
+            <div className='flex flex-col'>
             <img className='w-5 rounded-full' src={assets.admin_logo} alt="" />
+            <div className='text-blue-800 text-1xl justify-center'>ADMIN</div>
+            </div>
             <img className='w-8 rounded-full' src={userData.image} alt="" />
             <img className='w-2.5 rounded-full' src={assets.dropdown_icon} alt="" />
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
